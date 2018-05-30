fdfea55dea86c8c520f9abd1a4e2dc5e91f754ac
caleb woods <woodscaleb@gmail.com>
2018-05-30 10:12:35 -0700

    random password

--- app/models/sign_up.rb
+++ app/models/sign_up.rb
@@ -1,5 +1,5 @@
 class SignUp < ApplicationRecord
-	attr_accessor :password
+	before_validation {self.password = SecureRandom.hex(4)}
 	EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
 	validates :first_name, :presence => true
 	validates :last_name, :presence => true
