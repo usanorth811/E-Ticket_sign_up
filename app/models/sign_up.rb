8162bca2b80d949eca98a143b0fa11c88eee1446
caleb woods <woodscaleb@gmail.com>
2018-06-01 11:14:58 -0700

    fix phone validation

--- app/models/sign_up.rb
+++ app/models/sign_up.rb
@@ -3,7 +3,7 @@ class SignUp < ApplicationRecord
 	#include ActiveModel::Conversion
 	#extend ActiveModel::Naming
 	#attr_accessor :first_name, :Last_name, :Email, :company_name, :company_mailing_address, :city, :state, :zip, :company_phone_number, :password, :company_type, :user_name
-	
+
 	#before_validation {self.password = SecureRandom.hex(4)}
 	EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
 	validates :first_name, :presence => true
@@ -14,7 +14,7 @@ class SignUp < ApplicationRecord
 	validates :city, :presence => true
 	validates :state, :presence => true
 	validates :zip, :presence => true, :length =>{ :is => 5}
-	validates :company_phone_number, :presence => true, format: { with: /(?:(?:(\s*\(?([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\)?\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/}
+	validates :company_phone_number, :presence => true, format: { with: /[0-9]/}, :length =>{ :is => 10}
 	validates :password, :presence => true, :length =>{ :in => 6..14}, format: { with: /\A[\w\_]+\z/}
 	validates :company_type, :presence => true
 	validates :user_name, :presence => true, :uniqueness => true
