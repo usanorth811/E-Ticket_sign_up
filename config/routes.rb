344e25d86e887abdc3a39cf85b94baebd256d696
caleb woods <woodscaleb@gmail.com>
2018-06-11 09:17:27 -0700

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :sign_ups
-  root 'sign_ups#index'
+  root 'sign_ups#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
