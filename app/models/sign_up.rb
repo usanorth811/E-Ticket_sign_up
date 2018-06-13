267ff9077176395da6a434e11e71bd5213932525
caleb woods <woodscaleb@gmail.com>
2018-06-13 11:30:48 -0700

    alignment

--- app/models/sign_up.rb
+++ app/models/sign_up.rb
@@ -6,7 +6,7 @@ class SignUp < ApplicationRecord
 
 	#before_validation {self.password = SecureRandom.hex(4)}
 	attr_accessor :password_confirmation
-	no_whitespace = /\A\S+\Z/i
+	no_whitespace = /\A[\w\_]+\z/
 	EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
 	validates :first_name, :presence => true
 	validates :last_name, :presence => true
@@ -18,7 +18,7 @@ class SignUp < ApplicationRecord
 	validates :zip, :presence => true, :length =>{ :is => 5}
 	validates :company_phone_number, :presence => true, format: { with: /[0-9]/}, :length =>{ :is => 10}
 	validates :password, :confirmation => true, :presence => true, :length =>{ :in => 6..14}, format: { with: /\A[\w\_]+\z/}
-	validates :company_type, :presence => true, inclusion: { in: %w(Contractor Utility-Member)}
+	validates :company_type, :presence => true, inclusion: { in: %w(Contractor Member)}
 	validates :user_name, :presence => true, format: { with: no_whitespace}, :length =>{ :in => 6..14}
 	validates :password_confirmation, :presence => true
 		
